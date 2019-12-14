277dd7c3d124b9464279fc93a98e2c8bc3184e98
ArtyomViryutin <48865956+ArtyomViryutin@users.noreply.github.com>
2019-12-14 16:24:59 +0300

    Create Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,50 @@
+#include "Header.h"
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	system("chcp 1251");
+	system("cls");
+	Stack<Money, 5> f;
+	cout << "Работа с шаблоном, содержащим объекты класса Money" << endl;
+	cout << "Попробуем удалить элемент из стека до его инициализации:" << endl;
+	f.Pop();
+	Sep();
+	cout << "Инициализируем стек:" << endl;
+	cin >> f;
+	Sep();
+	cout << f;
+	Sep();
+	cout << "Удалим элемент из стека:" << endl;
+	f.Pop();
+	cout << f;
+	Sep();
+	cout << "Добавим элемент в стек (10, 11):" << endl;
+	Money m1(10, 11);
+	f.Push(m1);
+	cout << f;
+	Sep();
+	cout << "Попробуем добавить элемент в заполненный стек:" << endl;
+	f.Push(m1);
+	Sep();
+	cout << "Определим размер очереди и верхний элемент без его удаления" << endl;
+	cout << "Размер очереди - " << f.Size() << "\n" << "Верхний элемент - " << f.Top() << endl;
+	Sep();
+	cout << "Проверим работоспособность оператора присваивания:" << endl;
+	Stack<Money, 5> f1;
+	f1 = f;
+	cout << "Первый стек:" << endl;
+	cout << f;
+	cout << "Второй стек:" << endl;
+	cout << f1;
+	Sep();
+	cout << "Удалим из второго стека элемент и посмотрим не изменился ли первый стек:" << endl;
+	f1.Pop();
+	cout << "Первый стек:" << endl;
+	cout << f;
+	cout << "Второй стек:" << endl;
+	cout << f1;
+	system("pause");
+	return 0;
+}
+
+
